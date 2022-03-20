02adc5501fd3e4466de46a65ebc56df197ef125a
selahattin ceylan <selahattinceylan9622@gmail.com>
2022-03-20 22:20:50 +0300

    test

--- cmd/bitaksi-matching/bitaksi-matching.go
+++ cmd/bitaksi-matching/bitaksi-matching.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bitaksi/bitaksi-matching/internal/server"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	versionFlag    = flag.Bool("version", false, "Show version information.")
+	debugFlag      = flag.Bool("debug", false, "Show debug information.")
+	logFileFlag    = flag.String("log", "bitaksi-matching", "Path to the log file.")
+	configFileFlag = flag.String("config", "config.yml", "Path to the configuration file.")
+)
+
+func init() {
+	flag.Parse()
+
+	// Log settings
+	if *debugFlag {
+		logrus.SetReportCaller(true)
+		logrus.SetLevel(logrus.TraceLevel)
+	} else {
+		logrus.SetReportCaller(false)
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	logFile, err := os.OpenFile(*logFileFlag, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not open log file")
+	}
+	// logrus log file setted
+	logrus.SetOutput(logFile)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	if *versionFlag {
+		fmt.Fprintln(os.Stdout, version.Print("bitaksi-matching"))
+		os.Exit(0)
+	}
+
+	// Load configuration file
+	data, err := ioutil.ReadFile(*configFileFlag)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not load configuration")
+	}
+	var cfg server.Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not load configuration")
+	}
+
+	// Create server instance
+	instance := server.NewInstance(&cfg)
+
+	// Interrupt handler
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		logrus.Infof("Received %s signal", <-c)
+		instance.Shutdown()
+	}()
+
+	// Start server
+	logrus.Infof("Starting autoOrder %s", version.Info())
+	logrus.Infof("Build context %s", version.BuildContext())
+	instance.Start()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/Selahattinn/bitaksi-matching
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
